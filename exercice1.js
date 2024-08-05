965aff1b9a03aa8511f8a920f183baa308e0c163
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-08-05 00:24:24 +0000

    Modification de l'affichage sur la consol

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,12 @@
 //Caluculateur de pourboire
 
-const MONTANT_FACTURE = 140;
+const MONTANT_FACTURE = 150;
 
-const PourcentagePourboir = 15;
-let montantPourboir = (MONTANT_FACTURE *PourcentagePourboir)/100;
+const PourcentagePourboir = 10;
+let montantPourboir = (MONTANT_FACTURE * PourcentagePourboir)/100;
 
 let totalPaie = MONTANT_FACTURE + montantPourboir;
 
-console.log(montantPourboir);
-console.log(totalPaie);
+console.log(`La facture est de ${MONTANT_FACTURE}€`);
+console.log(`Le Pourboir est de ${montantPourboir}€`);
+console.log(`Le total Paie est  de ${totalPaie}`);
